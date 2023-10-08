8b195c0f1fd059dafa5d24427bc5f56e9299624b
AlexeiKonev <alexkonev777@gmail.com>
2023-10-08 13:25:56 +0300

    added Damage System for enemy

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -1,11 +1,17 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Serialization;
+namespace DefaultNamespace
+{
 public class Hammer : MonoBehaviour
 {
+    [SerializeField] private float damage = 20f;
+    
   [SerializeField] private Animator anim;
   [SerializeField] private string triggerName = "hammer";
+    [SerializeField] private string tagEnemy = "Enemy";
     // Start is called before the first frame update
     void Start()
     {
@@ -20,4 +26,21 @@ public class Hammer : MonoBehaviour
             Debug.Log("attack");
         }
     }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == tagEnemy)
+        {
+
+            if (other.gameObject.TryGetComponent<IDamage>(out IDamage enemy))
+            {
+                enemy.Damage(damage);   
+            }
+           
+            
+            
+        }
+    }
+}
+
 }
